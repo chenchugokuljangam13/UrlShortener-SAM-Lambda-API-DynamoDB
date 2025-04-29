be3234148ac2646f77c0336cfb1ff31e2a40cb9d
chenchugokuljangam13 <jangamchenchugokul@gmail.com>
2025-04-30 00:15:33 +0530

    app.ts file Update
    cleaned main lambda function by handlling the post and get methods by seperate funtion to the make code more readable

--- url-shortener/app.ts
+++ url-shortener/app.ts
@@ -1,12 +1,14 @@
-import { APIGatewayProxyEvent, APIGatewayProxyEventV2, APIGatewayProxyResult } from 'aws-lambda';
+import {APIGatewayProxyEventV2, APIGatewayProxyResult } from 'aws-lambda';
 import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
-import {DynamoDBDocumentClient, PutCommand, GetCommand} from '@aws-sdk/lib-dynamodb'
-import { error } from 'console';
-import { json } from 'stream/consumers';
-const client = new DynamoDBClient({
-    region: "us-east-1"
-})
+import {DynamoDBDocumentClient, PutCommand, GetCommand} from '@aws-sdk/lib-dynamodb';
+
+// Initializing DynamoDB clients
+const client = new DynamoDBClient({region: "us-east-1"})
 const docClient = DynamoDBDocumentClient.from(client);
+
+// Table name from DDB which is stored in Environment variables
+const tableName =  process.env.TABLE_NAME;
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -16,6 +18,8 @@ const docClient = DynamoDBDocumentClient.from(client);
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  *
  */
+
+// Generate a random 8-character alphanumeric short code if not given
 const getShortCode= (): string => {
     let chars = "";
     for (let i = 65; i <= 90; i++) chars += String.fromCharCode(i); 
@@ -31,122 +35,123 @@ const getShortCode= (): string => {
     return res;
 }
 
+// Handle POST request: store long URL with a short code and Gives Short Code as response
+const postRequestHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
+    const body = event.body ? JSON.parse(event.body) : event.queryStringParameters;
+    if (!body) {
+        return {
+            statusCode:400,
+            // Status code for Invalid request
+            body: JSON.stringify({
+                error: "Please give query"
+            })
+        }
+    }
+    const longUrl = body.longUrl;
+    if (!longUrl) {
+        return {
+            statusCode: 400,
+            // Status code for Invalid or Bad Request
+            body: JSON.stringify({
+                error: "Please enter the URL"
+            })
+        }
+    }
+    const shortCode = body.shortCode || getShortCode()
+    const params = {
+        TableName: tableName,
+        Item: {
+            short_code: shortCode,
+            long_url: longUrl
+        }
+    }
+    try {
+        await docClient.send(new PutCommand(params));
+        return {
+            statusCode: 201,
+            // status code for Created
+            body: JSON.stringify({
+                shortUrl: `https://${event.requestContext.domainName}/${event.requestContext.stage}/${shortCode}`
+            })
+        }
+    } catch(error) {
+        return {
+            statusCode: 500,
+            // status code for Internal Server Error
+            body: JSON.stringify({
+                error: "Internal Server Error"
+            })
+        }
+    }   
+}
 
+//Handle GET request: retrieve long URL using short code
+const getRequestHandler = async(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
+    const shortCode = event?.pathParameters?.shortCode
+    if (!shortCode) {
+        return {
+            statusCode: 400,
+                // status code for Bad request
+            body: JSON.stringify({
+             error: "Please enter shortCode"
+            })
+        }
+    }
+    const params = {
+        TableName: tableName,
+        Key: {
+        short_code: shortCode
+        }
+    }
+    try{
+        const record = await docClient.send(new GetCommand(params));
+        console.log(record)
+        if (!record.Item) {
+            return {
+                statusCode: 400,
+                // status code for Not Found
+                body: JSON.stringify({
+                    error: "Record not found please give valid shortCode"
+                })
+            }
+        }
+        return {
+            statusCode: 302,
+            // status code for Found
+            headers: {
+                Location: record.Item.long_url
+            },
+            body: JSON.stringify({
+                longUrl: record.Item.long_url
+            })
+        }
+    } catch(error) {
+        return {
+            statusCode: 500,
+            // status code for Internal Server Error
+            body: JSON.stringify({
+                error: "Internal server error"
+            })
+        }
+    }
 
+}
+   
+// main Lambda function to handel the API
 export const lambdaHandler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResult> => {
     console.log(event)
     const httpMethod = event.requestContext.http.method;
-    const tableName =  process.env.TABLE_NAME;
-    // console.log(tableName)
     if (httpMethod === 'POST') {
-        const body = event.body ? JSON.parse(event.body) : event.queryStringParameters
-        if (!body) {
-            return {
-                statusCode:400,
-                // Status code for Invalid request
-                body: JSON.stringify({
-                    error: "Please give query"
-                })
-            }
-        }
-        const longUrl = body.longUrl;
-        if (!longUrl) {
-            return {
-                statusCode: 400,
-                // Status code for Invalid or Bad Request
-                body: JSON.stringify({
-                    error: "Please enter the URL"
-                })
-            }
-        }
-        let shortCode;
-        if (body.shortCode){
-            shortCode = body.shortCode
-        } else {
-            shortCode = getShortCode();
-        }
-        
-        const params = {
-            TableName: tableName,
-            Item: {
-                short_code: shortCode,
-                long_url: longUrl
-            }
-        }
-        try {
-            await docClient.send(new PutCommand(params));
-            return {
-                statusCode: 201,
-                // status code for Created
-                body: JSON.stringify({
-                    shortUrl: `https://${event.requestContext.domainName}/${event.requestContext.stage}/${shortCode}`
-                })
-            }
-        } catch(error) {
-            return {
-                statusCode: 500,
-                // status code for Internal Server Error
-                body: JSON.stringify({
-                    error: "Internal Server Error"
-                })
-            }
-        }
+        return await postRequestHandler(event);
     }else if (httpMethod === 'GET'){
-        const shortCode = event?.pathParameters?.shortCode
-        console.log(shortCode)
-        if (!shortCode) {
-            return {
-                statusCode: 400,
-                // status code for Bad request
-                body: JSON.stringify({
-                    error: "Please enter shortCode"
-                })
-            }
-        }
-        const params = {
-            TableName: tableName,
-            Key: {
-                short_code: shortCode
-            }
-        }
-        try{
-            const record = await docClient.send(new GetCommand(params));
-            console.log(record)
-            if (!record.Item) {
-                return {
-                    statusCode: 400,
-                    // status code for Not Found
-                    body: JSON.stringify({
-                        error: "Record not found please give valid shortCode"
-                    })
-                }
-            }
-            return {
-                statusCode: 302,
-                // status code for Found
-                headers: {
-                    Location: record.Item.long_url
-                },
-                body: JSON.stringify({
-                    longUrl: record.Item.long_url
-                })
-            }
-        } catch(error) {
-            return {
-                statusCode: 500,
-                // status code for Internal Server Error
-                body: JSON.stringify({
-                    error: "Internal server error"
-                })
-            }
-        }
-
-    }
-    return {
-        statusCode: 405,
-        // status code for Method Not Allowed
-        body: JSON.stringify({
-            error: "This method is not allowed"
-        })
+        return await getRequestHandler(event);
+    } else {
+        return {
+            statusCode: 405,
+            // status code for Method Not Allowed
+            body: JSON.stringify({
+                error: "This method is not allowed"
+            })
+        }    
     }
 };
